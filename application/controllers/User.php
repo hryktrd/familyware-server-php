3b9c792ac6374b8e67fcca392f33eb1534e251c4
Hiroyuki Terada <hissan1983@gmail.com>
2017-07-26 17:20:41 +0900

    ユーザ名でのユーザ検索追加

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -15,17 +15,45 @@ class User extends CI_Controller
         $this->load->model('User_model');
     }
 
+    /**
+     * 全ユーザ取得
+     */
     public function getUsers()
     {
         $families = $this->User_model->users();
         echo json_encode($families);
     }
 
+    /**
+     * ユーザ名でユーザ検索
+     * @param $name ユーザ名
+     * @param bool $strict falseでLIKE使用、trueだと完全一致のみ
+     * @return mixed ユーザ情報
+     */
+    public function getUserByName($name, $strict = FALSE)
+    {
+        if(!$strict){
+            $this->db->like('name', $name, 'both');
+        }else {
+            $this->db->where('name', $name);
+        }
+
+        $query = $this->db->get('user');
+        return $query->result();
+    }
+
+    /**
+     * ユーザー情報取得
+     * @param $id ユーザID
+     */
     public function getUser($id){
         $family = $this->User_model->user($id);
         echo json_encode($family);
     }
 
+    /**
+     * ユーザー追加
+     */
     public function postUser()
     {
         $userInfo = json_decode(trim(file_get_contents('php://input')), true);
